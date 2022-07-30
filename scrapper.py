01438bff469a0a8de186405d83cf69f315c2d245
Felipe Faria <felipe.faria@waitrose.co.uk>
2022-07-30 22:23:21 +0100

    Fix get_mana method from spell.py

--- scrapper.py
+++ scrapper.py
@@ -48,7 +48,7 @@ for dnd_class in dnd_classes:
             while page <= max_page:
                 try:
                     base_url = f'https://dndtools.net/spells/?page={page}&class_levels__slug={dnd_class}&spellclasslevel__level={spell_level}'
-                    sleep(0.2)
+                    sleep(0.1)
                     driver.get(base_url)
                     max_page = int(driver.find_elements(By.CLASS_NAME, 'page')[-1].text)
                     spells = driver.find_elements(By.TAG_NAME, 'tr')
--- spell.py
+++ spell.py
@@ -5,7 +5,7 @@ def get_mana(spell_name, dnd_class):
     spells = json.load(open(dnd_class + '.json'))
 
     for spell in spells:
-        if spell['Spell name'] == spell_name:
-            return spell['Circle']
+        if spell['spell_name'] == spell_name:
+            return spell['spell_level']
 
     raise Exception("Spell not found")
